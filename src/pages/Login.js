17feb7228094b706af03819fc95954e23911b9c1
Heron Rodrigues <heronrs1@gmail.com>
2020-01-25 20:59:02 -0300

    styling login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { StyleSheet, TouchableOpacity, View, Text, Image } from 'react-native';
+
+import icon from './Spotify_Icon.png'
 
 export default function Login() {
-  return (
-    <div />
-  );
+    return (
+        <View style={styles.container}>
+            <Image source={icon} style={styles.image} />
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>FAÇA LOGIN NO SPOTIFY</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#191414',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    image: {
+        height: 80,
+        width: 80,
+        marginBottom: 50
+    },
+
+    button: {
+        height: 50,
+        width: 280,
+        backgroundColor: '#1DB954',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 25
+    },
+
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+});
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
 import Lyrics from './pages/Lyrics';
