f9515bf9b7eb5ee75bd836cb00cc6880ecc062e7
Heron Rodrigues <heronrs1@gmail.com>
2020-01-29 18:38:28 -0300

    saving user data

--- src/services/getAcessToken.js
+++ src/services/getAcessToken.js
@@ -6,22 +6,36 @@ const { spotifyCredentials } = credentials;
 
 const getAcessToken = async () => {
 
-    const responseAuthCode = await getAuthorizationCode();
-    const authorizationCode = responseAuthCode.params.code;
+    try{
+        const responseAuthCode = await getAuthorizationCode();
+        const authorizationCode = responseAuthCode.params.code;
 
-    const credsB64 = btoa(`${spotifyCredentials.clientId}:${spotifyCredentials.clientSecret}`);
+        const credsB64 = btoa(`${spotifyCredentials.clientId}:${spotifyCredentials.clientSecret}`);
+        
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                Authorization: `Basic ${credsB64}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${spotifyCredentials.redirectUri}`,
+        });
+        const responseJson = await response.json();
+        
+        const {
+            access_token: accessToken,
+            refresh_token: refreshToken,
+            expires_in: expiresIn,
+        } = responseJson;
     
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            Authorization: `Basic ${credsB64}`,
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${spotifyCredentials.redirectUri}`,
-    });
-    const responseJson = await response.json();
-    
-    return responseJson;
+        const expirationTime = new Date().getTime() + expiresIn * 1000;
+        await setUserData('accessToken', accessToken);
+        await setUserData('refreshToken', refreshToken);
+        await setUserData('expirationTime', expirationTime);
+        
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 export default getAcessToken;
